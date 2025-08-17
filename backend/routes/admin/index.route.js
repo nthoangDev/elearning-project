18439b51ba136353ab86aed11df79ce091e35dea
Nguyễn Thanh Hoàng <140046524+nthoangDev@users.noreply.github.com>
2025-08-17 15:47:23 +0700

    Revert "Feature/skeleton backend"

--- backend/routes/admin/index.route.js
+++
@@ -1,9 +0,0 @@
-const systemConfig = require("../../config/system");
-const courseRoutes = require('../admin/course.route');
-
-module.exports = (app)=>{
-    const PATH_ADMIN = systemConfig.prefixAdmin;
-    
-    app.use(`${PATH_ADMIN}/courses`, courseRoutes);
-}
-
